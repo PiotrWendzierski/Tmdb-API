232f218363f0c6e59bfb0e2869a8dfa16197f584
PiotrWendzierski <piotrwendzierski@gmail.com>
2025-02-21 00:17:40 +0100

    7. Edit FetchTmdbData

--- app/Console/Commands/FetchTmdbData.php
+++ app/Console/Commands/FetchTmdbData.php
@@ -20,15 +20,15 @@ class FetchTmdbData extends Command
 
         $tmdbService = new TmdbApiService();
 
-        // Fetch Movies in multiple languages
+        // fetch Movies in multiple languages
         foreach ($this->languages as $lang) {
             $this->info("Fetching movies in {$lang}...");
             $movies = $tmdbService->fetchMovies($lang);
 
-            // Sprawdzamy, czy zmienna $movies nie jest pusta
+            // scheck if movies is not empty
             if (empty($movies) || !isset($movies['results'])) {
                 $this->warn("No movie data found for language {$lang}.");
-                continue; // Pomiń ten język, jeśli brak danych
+                continue; // skip if no data
             }
 
             foreach ($movies['results'] as $movie) {
@@ -43,15 +43,15 @@ class FetchTmdbData extends Command
             }
         }
 
-        // Fetch Series in multiple languages
+        // detch Series in multiple languages
         foreach ($this->languages as $lang) {
             $this->info("Fetching series in {$lang}...");
             $series = $tmdbService->fetchSeries($lang);
 
-            // Sprawdzamy, czy zmienna $series nie jest pusta
+            // chceck if series is not empty
             if (empty($series) || !isset($series['results'])) {
                 $this->warn("No series data found for language {$lang}.");
-                continue; // Pomiń ten język, jeśli brak danych
+                continue; //skip if no data
             }
 
             foreach ($series['results'] as $serie) {
@@ -66,15 +66,15 @@ class FetchTmdbData extends Command
             }
         }
 
-        // Fetch Genres in multiple languages
+        // fetch Genres in multiple languages
         foreach ($this->languages as $lang) {
             $this->info("Fetching genres in {$lang}...");
             $genres = $tmdbService->fetchGenres($lang);
 
-            // Sprawdzamy, czy zmienna $genres nie jest pusta
+            // try if not empty
             if (empty($genres) || !isset($genres['genres'])) {
                 $this->warn("No genre data found for language {$lang}.");
-                continue; // Pomiń ten język, jeśli brak danych
+                continue; // skif if ...
             }
 
             foreach ($genres['genres'] as $genre) {
